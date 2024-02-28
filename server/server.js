454e50be7304d1c8dbf7b8e2307774fd3799e6df
Jay <jayantchoudhary271@gmail.com>
2024-02-28 09:49:36 +0530

    Changes in Code Structure

---
+++ server/server.js
@@ -0,0 +1,45 @@
+require("dotenv").config({ path: "./.env" });
+const express = require("express");
+const connectDB = require("./Utils/db");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+// Connect Database
+connectDB();
+
+const app = express();
+
+const corsOptions = {
+  origin: ["http://localhost:3000", "*"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+// app.use(cors({ origin: "*" }));
+app.use(cookieParser("secret"));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "5mb", extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "5mb", extended: true }));
+
+// Routers
+app.use("/api/auth", require("./Route/AuthRouter"));
+app.use("/api/collage", require("./Route/CollageRouter"));
+app.use("/api/course", require("./Route/CourseRouter"));
+
+const PORT = process.env.PORT || 4000;
+
+const server = app.listen(PORT, "0.0.0.0", (err) => {
+  if (err) throw err;
+  console.log(`==>> Ready on http://localhost:${PORT}`);
+});
+
+// DB error handler
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Log Error: ${err}`);
+  server.close(() => process.exit(1));
+});
