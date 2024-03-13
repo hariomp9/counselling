d82213daef9c746dfa2835caf75f1150ab73c57a
anurag-17 <anurag.pandey17@gmail.com>
2024-03-13 11:08:55 +0530

    update

--- src/app/user/navbar.js
+++ src/app/user/navbar.js
@@ -295,7 +295,7 @@ const Navbar = () => {
       <nav>
         <div className="navbar bg-base-100 border p-0 ">
           <div className="flex-1 w-1/2">
-            <div className="drawer  w-[20px] m-3 lg:hidden block">
+            <div className="drawer  w-[20px] m-3 mr-0 lg:hidden block">
               <input id="my-drawer" type="checkbox" className="drawer-toggle" />
               <div className="drawer-content">
                 <label htmlFor="my-drawer" className="drawer-button">
