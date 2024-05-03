e5b49752889a16020653a6185aeeb6af64aabb55
hariomp9 <hariompatil9@gmail.com>
2024-05-03 19:32:38 +0530

    update

--- src/components/user-profile/steps-pages/step-1/inner-pages/Neetdetails.jsx
+++ src/components/user-profile/steps-pages/step-1/inner-pages/Neetdetails.jsx
@@ -2,28 +2,26 @@ import React, { useState } from "react";
 import HeadTitle from "../HeadTitle";
 
 const Neetdetails = ({ onFormDataChange, studentDetail }) => {
-  // State to hold the form data
-
-  console.log(studentDetail);
   const [formData, setFormData] = useState({
     NEET_Details: {
-      FullName: "",
-      MobileNumber: "",
-      WhatsappNumber: "",
-      NEET_RegisterNumber: "",
-      RollNo: "",
-      Marks: "",
-      AllIndiaRank: "",
-      StateRank: "",
+      FullName: studentDetail?.FullName || "",
+      MobileNumber: studentDetail?.MobileNumber || "",
+      WhatsappNumber: studentDetail?.WhatsappNumber || "",
+      NEET_RegisterNumber: studentDetail?.NEET_RegisterNumber || "",
+      RollNo: studentDetail?.RollNo || "",
+      Marks: studentDetail?.Marks || "",
+      AllIndiaRank: studentDetail?.AllIndiaRank || "",
+      StateRank: studentDetail?.StateRank || "",
     },
   });
+
   const handleChange = (e) => {
-    // const { name } = e.target.value;
+    const { name, value } = e.target;
     setFormData({
       ...formData,
       NEET_Details: {
         ...formData.NEET_Details,
-        [e.target.value]: e.target.value,
+        [name]: value,
       },
     });
     // Pass the updated form data to the parent component
@@ -31,95 +29,134 @@ const Neetdetails = ({ onFormDataChange, studentDetail }) => {
       ...formData,
       NEET_Details: {
         ...formData.NEET_Details,
-        [e.target.value]: e.target.value,
+        [name]: value,
       },
     });
   };
+
   return (
     <>
       <HeadTitle title="NEET Details" />
       <div className=" ">
-        {/* Iterate over the inputFields array and create input elements */}
-
         <form className="grid grid-cols-3 gap-[24px] py-[16px]">
           <div className="col-span-1 profile_input_box">
             <label>Full Name</label>
             <input
               type="text"
-              placeholder="Enter FullName"
+              placeholder="Enter Full Name"
               name="FullName"
               onChange={handleChange}
-              defaultValue={studentDetail?.FullName}
+              // value={formData.NEET_Details.FullName}
+              value={
+                formData.NEET_Details.FullName
+                  ? formData.NEET_Details.FullName
+                  : studentDetail.FullName
+              }
             />
           </div>
           <div className="col-span-1 profile_input_box">
             <label>Mobile Number</label>
             <input
               type="text"
-              placeholder="Enter FullName"
+              placeholder="Enter Mobile Number"
               name="MobileNumber"
               onChange={handleChange}
-              defaultValue={studentDetail?.MobileNumber}
+              // value={formData.NEET_Details.MobileNumber}
+              value={
+                formData.NEET_Details.MobileNumber
+                  ? formData.NEET_Details.MobileNumber
+                  : studentDetail.MobileNumber
+              }
             />
           </div>
           <div className="col-span-1 profile_input_box">
             <label> Whatsapp Number</label>
             <input
               type="text"
-              placeholder="Enter FullName"
+              placeholder="Enter Whatsapp Number"
               name="WhatsappNumber"
               onChange={handleChange}
-              defaultValue={studentDetail?.WhatsappNumber}
+              // value={formData.NEET_Details.WhatsappNumber}
+              value={
+                formData.NEET_Details.WhatsappNumber
+                  ? formData.NEET_Details.WhatsappNumber
+                  : studentDetail.WhatsappNumber
+              }
             />
           </div>
           <div className="col-span-1 profile_input_box">
-            <label> NEET_RegisterNumber</label>
+            <label> NEET Register Number</label>
             <input
               type="text"
-              placeholder="Enter FullName"
+              placeholder="Enter NEET Register Number"
               name="NEET_RegisterNumber"
               onChange={handleChange}
-              defaultValue={studentDetail?.NEET_RegisterNumber}
+              // value={formData.NEET_Details.NEET_RegisterNumber}
+              value={
+                formData.NEET_Details.NEET_RegisterNumber
+                  ? formData.NEET_Details.NEET_RegisterNumber
+                  : studentDetail.NEET_RegisterNumber
+              }
             />
           </div>
           <div className="col-span-1 profile_input_box">
-            <label> RollNo</label>
+            <label> Roll No</label>
             <input
               type="text"
-              placeholder="Enter FullName"
+              placeholder="Enter Roll No"
               name="RollNo"
               onChange={handleChange}
-              defaultValue={studentDetail?.RollNo}
+              // value={formData.NEET_Details.RollNo}
+              value={
+                formData.NEET_Details.RollNo
+                  ? formData.NEET_Details.RollNo
+                  : studentDetail.RollNo
+              }
             />
           </div>
           <div className="col-span-1 profile_input_box">
             <label> Marks</label>
             <input
               type="text"
-              placeholder="Enter FullName"
+              placeholder="Enter Marks"
               name="Marks"
               onChange={handleChange}
-              defaultValue={studentDetail?.Marks}
+              // value={formData.NEET_Details.Marks}
+              value={
+                formData.NEET_Details.Marks
+                  ? formData.NEET_Details.Marks
+                  : studentDetail.Marks
+              }
             />
           </div>
           <div className="col-span-1 profile_input_box">
-            <label> AllIndiaRank</label>
+            <label> All India Rank</label>
             <input
               type="text"
-              placeholder="Enter FullName"
+              placeholder="Enter All India Rank"
               name="AllIndiaRank"
               onChange={handleChange}
-              defaultValue={studentDetail?.AllIndiaRank}
+              // value={formData.NEET_Details.AllIndiaRank}
+              value={
+                formData.NEET_Details.AllIndiaRank
+                  ? formData.NEET_Details.AllIndiaRank
+                  : studentDetail.AllIndiaRank
+              }
             />
           </div>
           <div className="col-span-1 profile_input_box">
-            <label> StateRank</label>
+            <label> State Rank</label>
             <input
               type="text"
-              placeholder="Enter FullName"
+              placeholder="Enter State Rank"
               name="StateRank"
               onChange={handleChange}
-              defaultValue={studentDetail?.StateRank}
+              // value={formData.NEET_Details.StateRank}
+              value={
+                formData.NEET_Details.StateRank
+                  ? formData.NEET_Details.StateRank
+                  : studentDetail.StateRank
+              }
             />
           </div>
         </form>
