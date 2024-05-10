334ad5528480881a5ead59efa11b7bc7c46fc75f
anurag-17 <anurag.pandey17@gmail.com>
2024-05-10 12:15:24 +0530

    added private couslling page

--- server/Model/User.js
+++ server/Model/User.js
@@ -33,12 +33,8 @@ const UserSchema = new mongoose.Schema(
 
     SubscriptionsPlan: [{
       type: String,
-<<<<<<< HEAD
       // enum: ["Free", "One-And-One", "Pro"],
       default: "Free",
-=======
-      default: "free",
->>>>>>> a1c1578f38454232d6194ed23a587bd7af449f0a
       required: true
     }],
 
