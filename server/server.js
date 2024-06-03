d199535a4ca5d34881cbeac92987f7b795ce611e
HarshalPP <130646816+HarshalPP@users.noreply.github.com>
2024-06-03 16:08:06 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,12 +10,12 @@ connectDB();
 
 const app = express();
 
-const corsOptions = {
-  origin: ["http://localhost:3000", "*"],
-  credentials: true,
-};
+// const corsOptions = {
+//   origin: ["http://localhost:3000", "*"],
+//   credentials: true,
+// };
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json());
 // app.use(cors({ origin: "*" }));
