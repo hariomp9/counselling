63ddae9941e9e27759def21b286a679f197b1a52
hariomp9 <hariompatil9@gmail.com>
2024-06-06 10:33:39 +0530

    update

---
+++ src/config/superAdProtectedR.js
@@ -0,0 +1,65 @@
+"use client";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import { setToken, removeToken } from "@/redux/adminSlice/authSlice";
+import Loader from "@/app/component/loader";
+import config from "@/config";
+
+const SuperAdProtectedR = (WrappedComponent) => {
+  const Wrapper = (props) => {
+    const router = useRouter();
+    const dispatch = useDispatch();
+    const userAuthToken = useSelector((state) => state.auth?.token);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isAuth, setIsAuth] = useState(false);
+
+    useEffect(() => {
+      const checkAuth = async () => {
+        if (!userAuthToken) {
+          router.push("/super-admin/superadmin-login");
+          return;
+        }
+        verify();
+      };
+      checkAuth();
+    }, [userAuthToken, router]);
+
+    const verify = async () => {
+      setIsLoading(true);
+      setIsAuth(false);
+      try {
+        const res = await axios.get(
+          `${config.baseURL}/api/auth/verifyUserToken/${userAuthToken}`
+        );
+        if (res.status === 200) {
+          setIsAuth(true);
+          setIsLoading(false);
+        } else {
+          dispatch(removeToken());
+          router.push("/super-admin/superadmin-login");
+          setIsLoading(false);
+        }
+      } catch (error) {
+        console.error("Error occurred:", error);
+        router.push("/super-admin/superadmin-login");
+        setIsLoading(false);
+      }
+    };
+
+    return (
+      <>
+        {isLoading ? (
+          <Loader />
+        ) : (
+          userAuthToken && isAuth && <WrappedComponent {...props} />
+        )}
+      </>
+    );
+  };
+
+  return Wrapper;
+};
+
+export default SuperAdProtectedR;
