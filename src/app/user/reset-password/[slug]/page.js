498ecd2847c0a60747a6f9003eb9a9dd9e47741b
hariomp9 <hariompatil9@gmail.com>
2024-06-14 12:30:22 +0530

    update

--- src/app/user/reset-password/[slug]/page.js
+++ src/app/user/reset-password/[slug]/page.js
@@ -1,20 +1,23 @@
 "use client";
-import config from "@/config";
 import React, { useState } from "react";
+import axios from "axios";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
+import config from "@/config";
+import { toast } from "react-toastify";
 
 const ResetPassword = ({ params }) => {
+  const router = useRouter();
   const [password, setPassword] = useState("");
   const [isLoading, setLoading] = useState(false);
-  const resetToken = params?.slug || "";
-  const router = useRouter();
+  const resetToken = params?.slug;
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
     try {
       const response = await axios.put(
-        `${config.baseURL}/api/auth/resetpassword`,
+        `http://35.154.216.63:4000/api/auth/resetpassword/${resetToken}`,
         { password: password },
         {
           headers: {
@@ -26,7 +29,8 @@ const ResetPassword = ({ params }) => {
 
       if (response.status === 200) {
         setLoading(false);
-        router.push("/");
+        router.push("/user/user-login");
+        toast.success("Password Update Successfully");
       } else {
         setLoading(false);
       }
@@ -36,40 +40,59 @@ const ResetPassword = ({ params }) => {
       setLoading(false);
     }
   };
+
   return (
     <>
-      <section>
-        <div className="flex justify-center my-20">
-          <div className="w-full md:w-[50%] mx-auto border text-center">
-            <h1 className="mb-2 2xl:text-[40px] sm:text-[35px] xl:text-[24px] md:text-[18px] text-[16px] xl:leading-[38px] font-bold">
-              Reset Password
-            </h1>
+      <div className="flex mt-20 ">
+        <div className="w-1/3 border rounded-lg p-5 mx-auto items-center shadow-black">
+          <div
+            className="flex justify-between items-center border border-[#f3f3f3] rounded-lg bg-white
+    2xl:px-5  2xl:h-[50px] 2xl:my-5
+    xl:px-4  xl:h-[40px] xl:my-2
+    lg:px-3  lg:h-[35px] lg:my-2
+    md:px-2  md:h-[30px] md:my-2
+    sm:px-1 sm:h-[25px] sm:my-2
+    px-1 h-[25px] my-2
+     "
+          >
+            <h2 className="2xl:text-[22px] xl:text-[18px] lg:text-[16px] md:text-[14px] sm:text-[12px] text-[10px] font-semibold ">
+              Change Password
+            </h2>
+          </div>
+          <div>
             <form
               onSubmit={handleSubmit}
-              className="w-full md:w-1/2 mx-auto px-[10px]"
+              className=" bg-white border  rounded-lg 2xl:p-2 xl:p-5  lg:p-1 md:p-2 p-1  mx-auto"
             >
-              <div className="md:py-2 mx-auto">
+              <div className="">
+                {/* <label
+                  className="custom_input_label"
+                >
+                  New Password
+                </label> */}
                 <input
                   type="password"
                   name="password"
-                  placeholder="New Password"
-                  className="login-input w-full mt-2 custom-input h-[35px] lg:h-[40px] xl:h-[50px] 2xl:h-[60px]"
                   onChange={(e) => setPassword(!password)}
+                  className="login-input w-full mt-2 custom-input h-[35px] lg:h-[40px]  xl:h-[50px] 2xl:h-[60px]"
                   required
+                  pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{8,}"
+                  title="Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character."
                 />
               </div>
 
-              <button
-                type="submit"
-                disabled={isLoading}
-                className="w-full bg-[#F38181] text-[14px] xl:text-[16px] 2xl:text-[18px] font-medium text-white p-2 rounded-lg hover:border hover:border-gray-300 h-[35px] lg:h-[40px] xl:h-[50px] 2xl:h-[60px] login-btn my-5"
-              >
-                {isLoading ? "Loading.." : "Submit"}
-              </button>
+              <div className="mt-4">
+                <button
+                  type="submit"
+                  className="w-full bg-[#0071BC] text-[14px] xl:text-[16px] 2xl:text-[18px] font-medium text-white p-2 rounded-lg hover:border hover:border-gray-300 h-[35px] lg:h-[40px]  xl:h-[50px] 2xl:h-[60px]"
+                >
+                  Update Password
+                </button>
+              </div>
             </form>
           </div>
         </div>
-      </section>
+      </div>
     </>
   );
 };
